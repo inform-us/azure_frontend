c3bda0a47e3db431f33ef884bc32738423371490
ruaridhg <ruaridhg@users.noreply.github.com>
2024-08-19 15:48:41 +0100

    Changed bash script to use gh token and re-generated workflow with correct name

--- set_up_github_secrets.sh
+++ set_up_github_secrets.sh
@@ -2,7 +2,9 @@
 
 SECRET_VALUE=$1
 RANDOM_NAME=$2
-gh auth login
+GITHUB_TOKEN=$3
+# gh auth login
+gh auth login --with-token $GITHUB_TOKEN
 gh secret set AZURE_STATIC_WEB_APPS_API_TOKEN -b$SECRET_VALUE
 
 cp templates/azure-swa-terraform.yml .github/workflows/azure-static-web-apps-$RANDOM_NAME.yml
